5cd7a50eb8a26fc47806b0a41cecbb5221b59a9b
velociraptor7447 <42496648+velociraptor7447@users.noreply.github.com>
2020-01-02 15:31:42 -0600

    First Commit

---
+++ src/test/java/com/example/bonjovi/BonjoviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bonjovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonjoviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
